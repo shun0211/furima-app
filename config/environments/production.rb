a118f544a95d452aca8c9f2c08b8c2d1cee06ed8
macha5a2 <58128795+macha5a2@users.noreply.github.com>
2020-02-16 15:50:23 +0900

    rename

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "mercari_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "ECapp_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
