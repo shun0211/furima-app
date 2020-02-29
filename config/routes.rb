a1c175642f7762a036fcedbd022eb321ae894ef4
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2020-03-01 08:14:20 +0900

    トップページ、商品詳細ページ、マイページのビュー仮置場作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'items/index'
-
+  resources :items, only: [:index, :show]
+  resources :users, only: [:index]
   root "items#index"
   get 'welcome/index'
 
-  root 'welcome#index'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
