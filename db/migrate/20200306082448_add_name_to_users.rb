c06f3fce28e38e0ce5b6869579019e5223fbedac
macha5a2 <58128795+macha5a2@users.noreply.github.com>
2020-03-07 11:58:55 +0900

    新規会員登録　view

---
+++ db/migrate/20200306082448_add_name_to_users.rb
@@ -0,0 +1,14 @@
+class AddNameToUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :familyname, :string, null: false
+    add_column :users, :firstname_kana, :string, null: false
+    add_column :users, :familyname_kana, :string, null: false
+    add_column :users, :phone_number, :integer, null:false
+    add_column :users, :profile_image, :string
+    add_column :users, :nickname, :string, null: false
+    add_column :users, :profile_text, :text
+    add_column :users, :birth_year, :integer, null:false
+    add_column :users, :birth_month, :integer, null:false
+    add_column :users, :birth_day, :integer, null:false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200301010829) do
+ActiveRecord::Schema.define(version: 20200306082448) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "firstname",                           null: false
+    t.string   "email",                                default: "", null: false
+    t.string   "encrypted_password",                   default: "", null: false
+    t.string   "firstname",                                         null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                                        null: false
+    t.datetime "updated_at",                                        null: false
+    t.string   "familyname",                                        null: false
+    t.string   "firstname_kana",                                    null: false
+    t.string   "familyname_kana",                                   null: false
+    t.integer  "phone_number",                                      null: false
+    t.string   "profile_image"
+    t.string   "nickname",                                          null: false
+    t.text     "profile_text",           limit: 65535
+    t.integer  "birth_year",                                        null: false
+    t.integer  "birth_month",                                       null: false
+    t.integer  "birth_day",                                         null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
