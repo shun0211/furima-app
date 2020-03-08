b2539cbf7b4fea241a1933e7e540bc3969bcecdc
ysmr0001 <adds.3.luvmid@gmail.com>
2020-03-08 18:29:40 +0900

    create credit_card model

---
+++ db/migrate/20200308092337_create_credit_cards.rb
@@ -0,0 +1,11 @@
+class CreateCreditCards < ActiveRecord::Migration[5.0]
+  def change
+    create_table :credit_cards do |t|
+      t.integer :number, null: false
+      t.integer :expiration_date, null: false
+      t.integer :security_code, null: false
+      # t.references :user_id, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
