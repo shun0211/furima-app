2e1aae94959f16a455170408a9fbcdc92ba16693
ysmr0001 <adds.3.luvmid@gmail.com>
2020-03-08 18:19:05 +0900

    create purchase_history model

---
+++ db/migrate/20200308091516_create_purchase_histories.rb
@@ -0,0 +1,9 @@
+class CreatePurchaseHistories < ActiveRecord::Migration[5.0]
+  def change
+    create_table :purchase_histories do |t|
+      # t.references :user_id, null: false, foreign_key: true
+      # t.references :item_id, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
