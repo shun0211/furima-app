46ee4d926fdce0a2864d40b8a7e675bc8631f701
ysmr0001 <adds.3.luvmid@gmail.com>
2020-03-08 14:25:07 +0900

    create like model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200307121400) do
+ActiveRecord::Schema.define(version: 20200308051338) do
 
   create_table "items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "product_name",                      null: false
@@ -25,6 +25,11 @@ ActiveRecord::Schema.define(version: 20200307121400) do
     t.datetime "created_at",                        null: false
     t.datetime "updated_at",                        null: false
     t.index ["product_name"], name: "index_items_on_product_name", using: :btree
+  end
+
+  create_table "likes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
