2a264b121bdbd42cc481616d8f08eb3c122122ff
ysmr0001 <adds.3.luvmid@gmail.com>
2020-03-08 19:13:49 +0900

    fixed indent

--- db/migrate/20200308093016_create_evaluations.rb
+++ db/migrate/20200308093016_create_evaluations.rb
@@ -4,7 +4,7 @@ class CreateEvaluations < ActiveRecord::Migration[5.0]
       t.integer :evaluation, null: false
       # t.references :user_id, foreign_key: true
       t.text :evaluation_comment
-      # t.references :item_id, null :false, foreign_key: true
+      # t.references :item_id, null: false, foreign_key: true
       t.timestamps
     end
   end
