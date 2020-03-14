d28e9b965c1110f5f805c396d2f2ad0633be389b
ysmr0001 <adds.3.luvmid@gmail.com>
2020-03-14 15:37:56 +0900

    add colon

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
-  belongs_to :buyer, class_name "User"
-  belongs_to :seller, class_name "User"
+  belongs_to :buyer, class_name: "User"
+  belongs_to :seller, class_name: "User"
   has_many :product_images
   has_many :likes
   has_many :comments
