19e5e1542c27c6cf821696537e9cac3084b4901a
macha5a2 <58128795+macha5a2@users.noreply.github.com>
2020-03-14 15:13:19 +0900

    部分テンプレート_header

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,11 @@ Rails.application.routes.draw do
       get :verification
     end
   end
-  resources :users, only: [:index, :new]
+  resources :users, only: [:index, :new] do
+    collection do
+      get 'sms'
+    end
+  end
   root "items#index"
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -71,17 +71,11 @@ ActiveRecord::Schema.define(version: 20200308093016) do
     t.string   "product_condition",                 null: false
     t.integer  "shipping_charge",                   null: false
     t.string   "days_of_ship",                      null: false
-    t.integer  "seller_id",                         null: false
-    t.integer  "buyer_id",                          null: false
     t.string   "brand"
     t.string   "size"
-    t.integer  "category_id",                       null: false
     t.datetime "created_at",                        null: false
     t.datetime "updated_at",                        null: false
-    t.index ["buyer_id"], name: "index_items_on_buyer_id", using: :btree
-    t.index ["category_id"], name: "index_items_on_category_id", using: :btree
     t.index ["product_name"], name: "index_items_on_product_name", using: :btree
-    t.index ["seller_id"], name: "index_items_on_seller_id", using: :btree
   end
 
   create_table "likes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -148,8 +142,6 @@ ActiveRecord::Schema.define(version: 20200308093016) do
   add_foreign_key "credit_cards", "users"
   add_foreign_key "evaluations", "items"
   add_foreign_key "evaluations", "users"
-  add_foreign_key "items", "users", column: "buyer_id"
-  add_foreign_key "items", "users", column: "seller_id"
   add_foreign_key "likes", "items"
   add_foreign_key "likes", "users"
   add_foreign_key "product_images", "items"
