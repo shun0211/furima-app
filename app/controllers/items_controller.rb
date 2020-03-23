147aa2357ec26918bc3f732f151f04af4cbed3dd
ysmr0001 <adds.3.luvmid@gmail.com>
2020-03-23 09:30:44 +0900

    add item.product_name,price,information

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,13 +4,37 @@ class ItemsController < ApplicationController
 
   def new
     @item = Item.new
+    @item.build_product_image
+  end
+
+  def create
+    Item.create(item_params)
+    redirect_to verification_items_path
   end
 
   def verification
   end
   
   def show
-    @items = Item.all
+    @item = Item.find(params[:id])
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(
+      :product_name,
+      :product_information,
+      :product_status,
+      :price,
+      :product_condition,
+      :shipping_charge,
+      :days_of_ship, 
+      :brand,
+      :size,
+      product_image_attributes: [:id, 
+                                 :image,
+                                 :item_id]
+    )
   end
   
 end
