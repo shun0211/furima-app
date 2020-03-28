a6b955a52844b37c9809d98061f9cfb3541dbb25
macha5a2 <58128795+macha5a2@users.noreply.github.com>
2020-03-28 12:26:07 +0900

    ログイン暫定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
 
-  private
-
   def production?
     Rails.env.production?
   end
@@ -14,4 +12,13 @@ class ApplicationController < ActionController::Base
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
+
+  def after_sign_in_path_for(resource)
+    root_url
+  end
+  
+  private
+    def sign_in_required
+      redirect_to new_user_session_url unless user_signed_in?
+    end
 end
