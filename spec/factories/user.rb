e25d9ee3b2b21cd51a36b72e8841b1ff400074e5
macha5a2 <58128795+macha5a2@users.noreply.github.com>
2020-04-11 11:38:10 +0900

    サインアップ　単体テスト

---
+++ spec/factories/user.rb
@@ -0,0 +1,18 @@
+FactoryBot.define do
+
+  factory :user do
+    nickname              {"macha"}
+    familyname          {"山田"}
+    firstname          {"太郎"}
+    familyname_kana           {"ヤマダ"}
+    firstname_kana           {"タロウ"}
+    birth_year              {"2000"}
+    birth_month              {"10"}
+    birth_day              {"10"}
+    email                 {"kkk@gmail.com"}
+    password              {"00000000"}
+    password_confirmation {"00000000"}
+    # tel             {"09000000000"}
+  end
+
+end
