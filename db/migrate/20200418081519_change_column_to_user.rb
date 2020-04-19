9c22582dc2a81e460286ea8a15f6cc5b403d470d
macha5a2 <58128795+macha5a2@users.noreply.github.com>
2020-04-19 09:44:23 +0900

    Update 20200418081519_change_column_to_user.rb

--- db/migrate/20200418081519_change_column_to_user.rb
+++ db/migrate/20200418081519_change_column_to_user.rb
@@ -1,5 +1,5 @@
 class ChangeColumnToUser < ActiveRecord::Migration[5.0]
-  def change
+  def up
     change_column :users, :phone_number, :string, presence: true, length: { maximum: 11 }
   end
 end
