46d8d07cd1d66a798d8c80a2b297d1af03cd2705
saoyun-k <12hiroki30@gmeil.com>
2020-04-19 12:05:20 +0900

    purchases commit2

---
+++ spec/factories/address.rb
@@ -0,0 +1,14 @@
+# FactoryBot.define do
+
+#   factory :address do
+#     familyname     {"山田"}
+#     firstname     {"彩"}
+#     familyname_kana     {"ヤマダ"}
+#     firstname_kana     {"アヤ"}
+#     postal_code     {"123-4567"}
+#     prefectures     {"北海道"}
+#     municipality     {"横浜市緑区"}
+#     address     {"青山1-1-1"}
+#   end
+
+# end
---
+++ spec/models/address_spac.rb
@@ -0,0 +1,70 @@
+# require 'rails_helper'
+# describe Address do
+#   describe '#create' do
+#     it "firstnameがない場合は登録できないこと" do
+#       address = build(:address, firstname: "")
+#       address.valid?
+#       expect(address.errors[:firstname]).to include("can't be blank")
+#     end
+
+#     it "familynameがない場合は登録できないこと"do
+#       address = build(:address, familyname: "")
+#       address.valid?
+#       expect(address.errors[:familyname]).to include("can't be blank")
+#     end
+
+#     it "firstname_kanaがない場合は登録できないこと" do
+#       address = build(:address, firstname_kana: "")
+#       address.valid?
+#       expect(address.errors[:firstname_kana]).to include("can't be blank")
+#     end
+
+#     it "firstname_kanaにカタカナ以外がある場合は登録できないこと " do
+#       address = build(:user, firstname_kana: "カナa")
+#       address.valid?
+#       expect(address.errors[:firstname_kana][0]).to include("is invalid")
+#     end
+
+#     it "familyname_kanaがない場合は登録できないこと" do
+#       address = build(:address, familyname_kana: "")
+#       address.valid?
+#       expect(address.errors[:familyname_kana]).to include("can't be blank")
+#     end
+
+#     it "familyname_kanaにカタカナ以外がある場合は登録できないこと " do
+#       address = build(:address, familyname_kana: "カナa")
+#       address.valid?
+#       expect(address.errors[:familyname_kana][0]).to include("is invalid")
+#     end
+
+#     it "postal_codeがない場合は登録できないこと" do
+#       address = build(:address, postal_code: "")
+#       address.valid?
+#       expect(address.errors[:postal_code]).to include("can't be blank")
+#     end
+
+#     it "postal_codeにハイフンがあり7桁であれば登録できること" do
+#       address = build(:address, postal_code: "1234567")
+#       address.valid?
+#       expect(address.errors[:postal_code]).to include()
+#     end
+
+#     it "prefecturesがない場合は登録できないこと" do
+#       address = build(:address, prefectures: "")
+#       address.valid?
+#       expect(address.errors[:prefectures]).to include("can't be blank")
+#     end
+
+#     it "municipalityがない場合は登録できないこと" do
+#       address = build(:address, municipality: "")
+#       address.valid?
+#       expect(address.errors[:municipality]).to include("can't be blank")
+#     end
+
+#     it "addressがない場合は登録できないこと" do
+#       address = build(:address, address: "")
+#       address.valid?
+#       expect(address.errors[:address]).to include("can't be blank")
+#     end
+#   end
+# end
