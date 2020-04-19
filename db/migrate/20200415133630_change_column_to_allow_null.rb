d305c6ab3c9df7916a2c871f3112f045641cc471
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2020-04-19 22:19:14 +0900

    微修正

--- db/migrate/20200415133630_change_column_to_allow_null.rb
+++ db/migrate/20200415133630_change_column_to_allow_null.rb
@@ -2,8 +2,4 @@ class ChangeColumnToAllowNull < ActiveRecord::Migration[5.0]
   def up
     change_column_null :users, :credit_card_id, true # null: trueを明示する必要がある
   end
-
-  # def down
-  #   change_column_null :users, :credit_card_id, false
-  # end
 end
