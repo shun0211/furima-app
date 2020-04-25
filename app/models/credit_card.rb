0ed8a794198e7f967dbd1a1ee7d67c646d5348dc
saoyun-k <12hiroki30@gmeil.com>
2020-04-25 12:07:30 +0900

    purchases commit8

--- app/models/credit_card.rb
+++ app/models/credit_card.rb
@@ -1,10 +1,5 @@
 class CreditCard < ApplicationRecord
   belongs_to :user
-
-    # バリデーション
-    validates :card_number, presence: true, length: { is: 16 }
-    validates :exp_month, :exp_year, presence: true
-    validates :cvc, presence: true, length: { is: 4 }
 end
 
 
